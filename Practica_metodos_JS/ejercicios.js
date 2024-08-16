c3310af481692c07bb845f9e5b249edf9102937e
Facundo Ramirez <rfacundo@e-contact.cl>
2024-08-16 11:37:27 -0300

    anexo ejercicios metodos JS

---
+++ Practica_metodos_JS/ejercicios.js
@@ -0,0 +1,29 @@
+// A CONTINUACIÓN SE DEJAN 3 ARREGLOS PARA PODER REALIZAR LOS EJERCICIOS.
+// RECOMIENDO CREAR UNA FUNCIÓN POR CADA RESOLUCION
+
+const nombres = ["Gerardo", "Francisca", "Carlos", "Juan", "Laura"];
+const numeros = [19, 5, 29, 32, -11, 25, 51, 46, -3, 36];
+const personal = [
+  { id: 1, nombre: "Juana", edad: 35, ingreso: 2530684 },
+  { id: 2, nombre: "Marcelo", edad: 41, ingreso: 2113942 },
+  { id: 3, nombre: "Juan", edad: 29, ingreso: 2358068 },
+  { id: 4, nombre: "Laura", edad: 19, ingreso: 1510123 },
+  { id: 5, nombre: "Carlos", edad: 23, ingreso: 2834572 },
+  { id: 6, nombre: "Juan Pablo", edad: 52, ingreso: 3268684 },
+  { id: 7, nombre: "Noelia", edad: 31, ingreso: 2600000 },
+  { id: 8, nombre: "Steven", edad: 20, ingreso: 1468276 },
+];
+
+// 1- Dado el listado de "nombres", sacar por consola los mismos nombres pero con todas sus letras en mayúscula
+
+// 2- Dado el listado de "números", filtrar por aquellos numeros pares que sean mayores a 25
+
+// 3- Dado el listado de "números", mapear el mismo devolviendo un nuevo listado con strings que digan "POSITIVO" o "NEGATIVO" según corresponda
+
+// 4- Dado el listado "personal", verificar si existe al menos uno/a que tenga ingresos mayores a 2600000
+
+// 5- Dado el listado "personal", verificar si todos/as tienen al menos 18 años
+
+// 6- Dado el listado "personal", mapear el mismo y devolver uno similiar pero con un aumento de los ingresos en un 50% de cada trabajador
+
+// 7- Dado el listado "personal", filtrar el mismo por aquellos trabajadores que tengan un ingreso menor a 2500000 y que tengan mas de 30 años
